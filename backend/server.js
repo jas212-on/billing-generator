dee13c99ea62d975600baf71c52cfeb1c4b407e7
Jason Bobby <jasonbobbym@gmail.com>
2025-12-27 20:15:27 +0530

    Added new features

--- backend/server.js
+++ backend/server.js
@@ -73,9 +73,11 @@ app.use("/api/admin",adminRoutes)
 
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
+
+
 
 httpServer.listen(PORT,()=>{
     console.log("Server starting at port "+PORT )
